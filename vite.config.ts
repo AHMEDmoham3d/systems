6a07c1cd3278e965b6d7df8a4d45dc030c3f41c9
Ahmed Mohamed <162385026+AHMEDmoham3d@users.noreply.github.com>
2025-12-29 15:51:08 +0200

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: './', // 🔥 مهم جدًا
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
 });
