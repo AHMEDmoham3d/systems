57a4692e57be2702874b163188e4b373f6a58370
Ahmed Mohamed <162385026+AHMEDmoham3d@users.noreply.github.com>
2025-12-29 15:41:04 +0200

    first commit

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+const supabaseUrl = 'https://qnozlrgdqrnayuixtwmd.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFub3pscmdkcXJuYXl1aXh0d21kIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY0NzEzNTIsImV4cCI6MjA3MjA0NzM1Mn0.GeAFug9yoKYoGmXE3kgC4WsdVu08KHarr-tMbsaYDyo'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
